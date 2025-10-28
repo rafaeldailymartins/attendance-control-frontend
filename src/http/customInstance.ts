f0b2f3e4a97436150a24576ef1fd0590fe81fc19
rafaeldailymartins <rafaeldaily@outlook.com>
2025-10-27 22:00:33 -0300

    🔧 chore: move api url to .env files

--- src/http/customInstance.ts
+++ src/http/customInstance.ts
@@ -3,7 +3,7 @@ import { storage } from "@/lib/storage";
 import type { ApiError } from "./gen/api.schemas";
 
 export const AXIOS_INSTANCE = Axios.create({
-	baseURL: "http://localhost:8000",
+	baseURL: import.meta.env.VITE_API_URL,
 });
 
 AXIOS_INSTANCE.interceptors.request.use((config) => {
